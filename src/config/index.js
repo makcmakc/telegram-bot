70871cee4e2aa61a25e2a1d3363cba1e49e36b77
maximillian <makcmakc321@mail.ru>
2020-07-12 22:02:51 +0300

    Finish base template

---
+++ src/config/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+	TOKEN: '778712059:AAHoW6ymna_8fniIX-Nd-6SCJSX_0Ie49lw'
+}
---
+++ src/index.js
@@ -0,0 +1,44 @@
+const TelegramBot = require('node-telegram-bot-api')
+const config = require('./config')
+const helper = require('./utils/index')
+const kb = require('./utils/keyboard.buttons')
+const keyboard = require('./utils/keyboard')
+
+helper.logStart()
+
+const bot = new TelegramBot(config.TOKEN, {
+  polling: true
+})
+
+bot.on('message', msg => {
+
+	const chatId = helper.getChatId(msg)
+
+	switch(msg.text) {
+		case kb.home.favorite:
+			break
+		case kb.home.films:
+			bot.sendMessage(chatId, 'Выберите жанр: ', {
+				reply_markup: {keyboard: keyboard.films}
+			})
+			break
+		case kb.home.cinemas:
+			break						
+		case kb.back:
+			bot.sendMessage(chatId, 'Что хотите посмотреть?', {
+				reply_markup: {keyboard: keyboard.home}
+			})		
+			break						
+	}
+
+})
+
+bot.onText(/\/start/, msg => {
+	const text = `Здравствуйте, ${msg.from.first_name}\nВыберите команду для началы работы: `
+
+	bot.sendMessage(helper.getChatId(msg), text, {
+		reply_markup: {
+			keyboard: keyboard.home
+		}
+	})
+})
